479a81c292bf123de7b20b859ec78ab1cf3abb48
q-contradiction <thanoslivanis@gmail.com>
2024-07-21 20:47:37 +0100

    over-under

--- fair props.R
+++ fair props.R
@@ -1,11 +1,12 @@
 # Evaluating different methods to remove bookmaker's overround in football odds
 
 # A reproduction in R of the methodology from: 
-# Wisdom of Crowd-Joseph Buchdahl (www.football-data.co.uk/blog/wisdom_of_the_crowd.php)
+# Wisdom of Crowd - Joseph Buchdahl (www.football-data.co.uk/blog/wisdom_of_the_crowd.php)
 
-# Additional methods included in the implied package
+# Additional methods included in the implied package,
 # applying same logic to different bookmakers and over-under odds.
-# and find the optimal method to arrive at a set of fair probabilities
+
+# Find the optimal method to arrive at a set of fair probabilities
 # using rank probability score, linear regression and returns from fair odds
 
 # Data from football-data.co.uk
@@ -13,27 +14,27 @@
 suppressPackageStartupMessages({
   library(implied) #https://opisthokonta.net/?p=1797
   library(microbenchmark)
+  library(scales)
   library(broom)
   library(tidyverse)
 })
 
 rm(list = ls())
-options(scipen = 999)
 
 # Data --------------------------------------------------------------------
 
 football_odds <- readRDS(file = "./data-raw/football-data.rds")
 
-# Odds Abbreviations for 1x2 in football-data.co.uk
-odds_1X2 <- list(Bet365 = c("B365H", "B365D", "B365A"), Bwin = c("BWH", "BWD", "BWA"),
+# odds abbreviations for 1x2 in football-data.co.uk
+odds_1x2 <- list(Bet365 = c("B365H", "B365D", "B365A"), Bwin = c("BWH", "BWD", "BWA"),
                  Interwetten =  c("IWH", "IWD", "IWA"), Pinnacle = c("PSH", "PSD", "PSA"), 
                  WilliamHill = c("WHH", "WHD", "WHA"),  VC = c("VCH", "VCD", "VCA"), 
                  Sportingbet =  c("SBH", "SBD", "SBA"), Blue_Square =  c("BSH", "BSD", "BSA")) 
 
-# Odds Abbreviations for OU
-odds_OU <- list(Bet365 = c("B365>2.5", "B365<2.5"), Pinnacle = c("P>2.5", "P<2.5"))
+# odds Abbreviations for over-under
+odds_ou <- list(Bet365 = c("B365>2.5", "B365<2.5"), Pinnacle = c("P>2.5", "P<2.5"))
 
-odds_abbs <- list('1X2'= odds_1X2, OU = odds_OU)
+odds_abbs <- list('1X2'= odds_1x2, OU = odds_ou)
 
 football_odds <- suppressWarnings(mutate_at(football_odds, vars(unlist(odds_abbs)), as.numeric))
 
@@ -42,18 +43,18 @@ football_odds <- football_odds %>%
   mutate(ID = as.numeric(rownames(.)),
          FTL = ifelse(FTHG + FTAG > 2, "O", "U"))
 
-n_obs <- colMeans(!is.na(football_odds[, unlist(c(map(odds_1X2, 1),map(odds_OU, 1)))])) %>% 
+n_obs <- colMeans(!is.na(football_odds[, unlist(c(map(odds_1x2, 1),map(odds_ou, 1)))])) %>% 
   round(2) %>% sort(decreasing = T)
 
 max_odds <- football_odds %>%
-  select(all_of(unlist(odds_1X2))) %>% lapply(max, na.rm = T) 
+  select(all_of(unlist(odds_1x2))) %>% lapply(max, na.rm = T) 
 
 # Methods -----------------------------------------------------------------
 
 # methods supported in 'implied_probabilities' from implied Package
 methods <- c("basic", "wpo", "bb", "or", "power", "shin", "jsd", "additive")
 
-## microbenchmark ----
+## Complexity ----
 
 x <- c(1.5, 3.5, 8)
 
@@ -65,7 +66,7 @@ microbenchmark(list = lapply(methods, function(method) {bquote(implied_probabili
   ggplot(aes(x = method, y = log2(time), fill = method)) + 
   geom_boxplot() + theme_bw() + guides(fill = "none")
 
-# applied margin ----
+## Applied margin ----
 
 # mapping fair probabilities to overround probabilities, based on & method & number of events & margin 
 odds_mapper <- function(method, events, margin = 0.08) {
@@ -88,11 +89,12 @@ odds_mapper <- function(method, events, margin = 0.08) {
   return (odds)
 }
 
-# method jsd is not supported with 'implied_odds', additive same transformation as wpo, exclude both
+# jsd is not supported with 'implied_odds', additive same transformation as wpo, exclude both
 odds <- data.frame(method = rep(methods[1:6], each = 2),
                    events = c(2, 3)) %>%
   pmap_dfr(odds_mapper)
 
+# wpo and shin identical mapping in 2 events
 odds %>% 
   ggplot(aes(x = fair_props, y = 1/odds - fair_props, color = method)) +
   geom_point(aes(alpha = events)) +
@@ -105,8 +107,6 @@ odds %>%
 # Fair probabilities ------------------------------------------------------
 
 fair_probabilities <- function(method, bookmaker, market) {
-  
-  if (market == "OU" && method == "shin") return (NULL)
   
   params <- as.list(environment())
   
@@ -133,13 +133,25 @@ fair_probabilities <- function(method, bookmaker, market) {
   return (bind_cols(params, df[is_ok, ], fair_props[is_ok, ]))
 }
 
-props_table <- expand.grid(bookmaker = c("Pinnacle", "Bet365", "Bwin", "WilliamHill"),
-                           method = methods[1:6], 
-                           stringsAsFactors = F) %>%
+props_table_1x2 <- expand.grid(bookmaker = c("Pinnacle", "Bet365", "Bwin", "WilliamHill"),
+                               method = methods[1:6], 
+                               stringsAsFactors = F) %>%
   pmap_dfr(fair_probabilities, market = "1X2", .progress = T)
 
-## rank probability score ----
-cum_props <- t(apply(data.matrix(select(props_table, PH, PD, PA)), 1, cumsum))
+props_table_ou <- expand.grid(bookmaker = c("Pinnacle", "Bet365"),
+                              method = methods[1:5], 
+                              stringsAsFactors = F) %>%
+  pmap_dfr(fair_probabilities, market = "OU", .progress = T)
+
+props_table <- list("1X2" = props_table_1x2, "OU" = props_table_ou)
+
+# free memory
+rm(list = c("props_table_1x2", "props_table_ou"))
+
+# Evaluation --------------------------------------------------------------
+
+## A. Rank probability score (1x2), Brier Score (OU) ----
+cum_props <- t(apply(data.matrix(select(props_table[["1X2"]], PH, PD, PA)), 1, cumsum))
 
 res <- tribble(
   ~Res, ~r1, ~r2, ~r3,
@@ -148,18 +160,27 @@ res <- tribble(
   "A" ,  0 ,  0 ,  1 
 )
 
-res_props <- left_join(props_table, res, by = "Res") %>%
+res_props <- left_join(props_table[["1X2"]], res, by = "Res") %>%
   select(r1, r2, r3) %>% data.matrix()
 
-props_table$RPS <- 0.5*rowSums((cum_props - res_props)^2)
+props_table[["1X2"]]$RPS <- 0.5*rowSums((cum_props - res_props)^2)
 
-rank_probability_score <-  props_table %>%
+rank_probability_score <-  props_table[["1X2"]] %>%
   group_by(bookmaker, method, market) %>%
   summarize(rps_avg = mean(RPS), .groups = "drop") %>%
   arrange(rps_avg)
 
-## actual & implied prob (regression) ----
-props_long <- props_table %>%
+brier_score <- props_table[["OU"]] %>%
+  mutate(BS = case_when(
+    Res == "O" ~ (1 - PO)^2, 
+    Res == "U" ~ (1 - PU)^2,
+    NA ~ NA)) %>%
+  group_by(bookmaker, method, market) %>%
+  summarize(bs_avg = mean(BS), .groups = "drop") %>%
+  arrange(bs_avg)
+
+## B. Actual & Implied prob (regression) ----
+props_long <- props_table[["1X2"]] %>%
   pivot_longer(cols = c(PH, PD, PA), names_to = "selection", values_to = "implied_probability") %>%
   mutate(evaluation = (Res == substr(selection, 2, 2))) %>%
   group_by(bookmaker, method, market) %>%
@@ -176,6 +197,7 @@ reg <- props_long %>%
          adj_r_squared = map_dbl(model, ~ glance(.x) %>% pull(adj.r.squared))) %>%
   select(bookmaker, method, market, coef, adj_r_squared) %>%
   unnest(cols = c(coef)) %>%
+  # deviation from optimal line y = x
   mutate(dev = abs(estimate - 1)) %>%
   arrange(dev)
 
@@ -185,8 +207,7 @@ ggplot(props_long, aes(x = implied_prop, y = actual_prop, color = method)) +
   facet_wrap(bookmaker ~.) + theme_bw() +
   labs(x = "Odds implied Probability", y = "Actual Probability")
 
-## fair returns ----
-
+## C. Fair PnL & Yield ----
 fair_returns <- function(props) {
   
   nevents = ifelse(unique(props$market) == "1X2", 3, 2)
@@ -201,19 +222,32 @@ fair_returns <- function(props) {
     group_by(bookmaker, method, market) %>%
     mutate(NBets = seq(nevents, by = nevents, length.out = n()), 
            fair_PnL_r = cumsum(fair_PnL), 
-           yield = fair_PnL_r/NBets)
+           yield = fair_PnL_r/NBets, 
+           .keep = "used") %>%
+    ungroup()
   
   return (fair_bets)
 }
 
-fair_bets <- fair_returns(props_table) 
+fair_bets <- map_dfr(props_table, fair_returns) 
 
+# power and then wpo more suitable for 1x2 - bb for OU
 fair_bets %>% 
-  filter(NBets > 0) %>% 
   ggplot(aes(x = NBets, y = fair_PnL_r, color = method)) + 
   geom_line() + 
   geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.5) +
   facet_wrap(bookmaker ~ market, scales = "free") +
-  theme_bw() +
-  labs(title = "Profit n Loss with fair odds from different bookies",
-       subtitle = "Different ovverround removal methods")
+  theme_bw() + scale_x_continuous(labels = label_number(scale = 1e-3, suffix = "k")) +
+  labs(title = "Profit n Loss with fair odds on all outcomes",
+       subtitle = "Different overround removal methods")
+
+fair_bets %>%
+  filter(market == "1X2") %>%
+  group_by(bookmaker, method) %>%
+  slice_max(NBets) %>%
+  ungroup() %>%
+  ggplot(aes(x = method, y = yield, fill = bookmaker)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") + 
+  theme_bw() + scale_y_continuous(labels = percent_format()) +
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_text(size = 12))
